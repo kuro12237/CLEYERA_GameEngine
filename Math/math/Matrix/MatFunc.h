b76f666c1904833fffdc4be9fe74eeab44c7db52
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2025-03-10 12:53:19 +0900

    mathヲ少し拡張

--- Math/math/Matrix/MatFunc.h
+++ Math/math/Matrix/MatFunc.h
@@ -3,7 +3,10 @@
 
 namespace Math::Matrix::Func {
 
-
+	/// <summary>
+	/// 3x4s—ñ’PˆÊs—ñ
+	/// </summary>
+	/// <returns></returns>
 	Mat3x4 Identity3x4();
 
 
--- Math/math/Matrix/SMatrix.h
+++ Math/math/Matrix/SMatrix.h
@@ -1,7 +1,30 @@
 #pragma once
+#include<cassert>
 
 namespace Math::Matrix {
 
+/// <summary>
+/// 4x4行列
+/// </summary>
+struct Mat4x4 {
+
+  float m[4][4];
+
+  /// <summary>
+  /// 単位行列
+  /// </summary>
+  void Identity();
+
+  /// <summary>
+  /// 逆行列
+  /// </summary>
+  Mat4x4 Inverse();
+
+};
+
+/// <summary>
+/// 3x4行列
+/// </summary>
 struct Mat3x4 {
 
   float m[3][4];
@@ -11,7 +34,12 @@ struct Mat3x4 {
   /// </summary>
   void Identity();
 
-  void Update();
+
+  /// <summary>
+  /// 4x4からの変換
+  /// </summary>
+  void FromMat4x4(const Mat4x4 &mat);
+
 
 };
 
