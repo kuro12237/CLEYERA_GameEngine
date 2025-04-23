b96cab0ef6e23d0076b28775e2de3be5220d0416
翼 茂木 <k022g0130@m.neec.ac.jp>
2025-04-23 14:31:19 +0900

    プレイヤーを動かせれるようにする

--- GameEngine/App/GameObject/Player/PlayerManager.cpp
+++ GameEngine/App/GameObject/Player/PlayerManager.cpp
@@ -30,6 +30,38 @@ void PlayerManager::Init() {
 
 void PlayerManager::Update() {
 
+    Math::Vector::Vec3 direction = {}; // 入力方向ベクトル
+
+    float baseSpeed = 0.1f;
+    if ( inputManager_->PushKey(DIK_LSHIFT) ) {
+        baseSpeed *= 5.0f;
+    }
+    // 入力方向をベクトルで表現
+    if ( inputManager_->PushKey(DIK_W) )
+        direction.z += 1.0f;
+    if ( inputManager_->PushKey(DIK_S) )
+        direction.z -= 1.0f;
+    if ( inputManager_->PushKey(DIK_D) )
+        direction.x += 1.0f;
+    if ( inputManager_->PushKey(DIK_A) )
+        direction.x -= 1.0f;
+
+    // 正規化（斜め移動を速くしすぎないように）
+    float len = std::sqrt(direction.x * direction.x + direction.y * direction.y + direction.z * direction.z);
+    if ( len > 0.0f ) {
+        direction.x /= len;
+        direction.y /= len;
+        direction.z /= len;
+    }
+    force_.x = direction.x * baseSpeed;
+    force_.z = direction.z * baseSpeed;
+
+    if ( inputManager_->PushKeyPressed(DIK_SPACE) ) {
+
+        velocity_.y += 3.0f;
+    }
+
+
    handler_->Handler();
 
    handler_->Exec();
