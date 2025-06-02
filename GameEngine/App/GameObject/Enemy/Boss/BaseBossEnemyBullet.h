4e07190aaaf243f178691567738edc8202089512
翼 茂木 <k022g0130@m.neec.ac.jp>
2025-06-02 14:29:42 +0900

    保留

---
+++ GameEngine/App/GameObject/Enemy/Boss/BaseBossEnemyBullet.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @file BaseBossEnemyBullet.h
+ * @brief ボスの弾クラス
+ * @author 茂木翼
+ */
+
+
+#include"CLEYERA.h"
+#include "Enemy/EnemyParameter.h"
+#include "Enemy/Boss/Behavior/BossEnemyBehaviorNode.h"
+
+
+/// <summary>
+/// ボスの弾
+/// </summary>
+class BaseBossEnemyBullet : public CLEYERA::Component::ObjectComponent {
+
+};
