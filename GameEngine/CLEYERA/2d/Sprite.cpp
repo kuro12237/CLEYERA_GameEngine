a16249d96658d4edb1bfedb71e27d8cec0aa864f
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2025-06-20 12:20:53 +0900

    一旦上げる meshの基礎クラスまで

---
+++ GameEngine/CLEYERA/2d/Sprite.cpp
@@ -0,0 +1,9 @@
+#include "Sprite.h"
+
+CLEYERA::Sprite2d::Sprite::Sprite() { cameraManager = Manager::CameraManager::GetInstance(); }
+
+void CLEYERA::Sprite2d::Sprite::Create() {}
+
+void CLEYERA::Sprite2d::Sprite::TransfarBuf() {}
+
+void CLEYERA::Sprite2d::Sprite::Draw() {}
--- GameEngine/CLEYERA/2d/Sprite.h
+++ GameEngine/CLEYERA/2d/Sprite.h
@@ -1,12 +1,15 @@
 #pragma once
+#include "Utility/Camera/CameraManager.h"
 #include "pch/pch.h"
 
+namespace CLEYERA {
+namespace Sprite2d {
 /// <summary>
 /// 2dSpriteClass
 /// </summary>
 class Sprite {
 public:
-  Sprite() {};
+  Sprite();
   ~Sprite() {};
 
   void Create();
@@ -16,10 +19,21 @@ public:
   void Draw();
 
 #pragma region Set
-  void SetTexHandle(const uint32_t &handle);
+  void SetSRT(const Math::Vector::Vec3 &s, const Math::Vector::Vec3 &r,
+              const Math::Vector::Vec3 &t) {
+    this->scale_ = &s;
+    this->rotate_ = &r;
+    this->translate_ = &t;
+  }
+  void SetTexHandle(const uint32_t &handle) { this->texHandle_ = &handle; };
+  void SetSize(const Math::Vector::Vec2 &size) { this->size_ = &size; };
+  void SetAnker(const Math::Vector::Vec2 &anker) { this->anker_ = &anker; };
+
 #pragma endregion
 
 private:
+  Manager::CameraManager *cameraManager = nullptr;
+
   const Math::Vector::Vec3 *scale_ = nullptr;
   const Math::Vector::Vec3 *rotate_ = nullptr;
   const Math::Vector::Vec3 *translate_ = nullptr;
@@ -31,3 +45,5 @@ private:
 
   // Buf
 };
+} // namespace Sprite2d
+} // namespace CLEYERA
